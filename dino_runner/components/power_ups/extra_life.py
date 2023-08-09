5636d1056a910951a2ed3c1fb5720f89999fe45b
Ângelo Barbosa <129123574+AnxeLOo@users.noreply.github.com>
2023-08-09 11:21:18 -0300

    Delete dino_runner directory

--- dino_runner/components/power_ups/extra_life.py
+++
@@ -1,7 +0,0 @@
-from dino_runner.utils.constants import HEART, DEFAULT_TYPE
-from dino_runner.components.power_ups.power_up import PowerUp
-
-
-class ExtraLife(PowerUp):
-    def __init__(self):
-        super().__init__(HEART, DEFAULT_TYPE)
